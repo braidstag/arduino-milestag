d2ebd98637811892014143deaa11687eed3f74e4
Andrew Shirley <andrew@als-bi.com>
2021-02-19 21:35:58 +0000

    Add a headless mode
    This doesn't require an X server or Qt libraries

--- game/server.py
+++ game/server.py
@@ -5,20 +5,21 @@ from __future__ import print_function
 import argparse
 import sys
 import json
+import signal
+from time import sleep
 
 from gameState import GameState
 from gameLogic import GameLogic
 from serverConnection.listeningThread import ListeningThread
-from serverUi import MainWindow
 from player import Player
 import proto
 
 from api.restapi import RestApiThread
 
-from PySide import QtGui
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='BraidsTag server.')
+  parser.add_argument('-H', '--headless', action='store_true', help='start headless. There will be no UI although the REST API will still allow control')
+
   args = parser.parse_args()
 
   gameState = GameState()
@@ -49,20 +50,34 @@ if __name__ == '__main__':
     playerAdjusted = playerAdjusted
   )
 
-  # Create Qt application
-  app = QtGui.QApplication(sys.argv)
-  mainWindow = MainWindow(gameLogic, gameState, main)
-  mainWindow.show()
+  def exit(retval):
+    def printPlayers(s):
+      for i in s.players.values():
+        print(i)
+    gameState.withCurrGameState(printPlayers)
 
-  # Enter Qt main loop
-  retval = app.exec_()
+    main.stop()
+    api.stop()
 
-  def printPlayers(s):
-    for i in s.players.values():
-      print(i)
-  gameState.withCurrGameState(printPlayers)
+    sys.exit(retval)
 
-  main.stop()
-  api.stop()
+  if not args.headless:
+    from serverUi import MainWindow
+    from PySide import QtGui
 
-  sys.exit(retval)
+    # Create Qt application
+    app = QtGui.QApplication(sys.argv)
+    mainWindow = MainWindow(gameLogic, gameState, main)
+    mainWindow.show()
+
+    # Enter Qt main loop
+    retval = app.exec_()
+
+    exit(retval)
+  else:
+    signal.signal(signal.SIGINT, lambda x,y: exit(1))
+
+    #kill time until we are interrupted.
+    while True:
+      sleep(3600)
+
